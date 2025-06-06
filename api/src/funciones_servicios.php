245925bcf03695d8e975c8cd8c6ca5b05bb99a5a
fmilort <fmilort@g.educaand.es>
2025-06-06 16:56:51 +0100

    backend_finished

--- api/src/funciones_servicios.php
+++ api/src/funciones_servicios.php
@@ -9,56 +9,6 @@ define("SERVIDOR_BD","localhost");
 define("USUARIO_BD","root");
 define("CLAVE_BD","");
 define("NOMBRE_BD","bd_lvup");
-define("PASSWORD_API","PASSWORD_DE_MI_APLICACION");
-
-function validateToken()
-{
-    $headers = apache_request_headers();
-    if (!isset($headers["Authorization"]))
-        return false; // Sin autorización
-    else {
-        $authorization = $headers["Authorization"];
-        $authorizationArray = explode(" ", $authorization);
-        $token = $authorizationArray[1];
-        try {
-            $info = JWT::decode($token, new Key(PASSWORD_API, 'HS256'));
-        } catch (\Throwable $th) {
-            return false; // Expirado
-        }
-
-        try {
-            $conexion = new PDO("mysql:host=" . SERVIDOR_BD . ";dbname=" . NOMBRE_BD, USUARIO_BD, CLAVE_BD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-        } catch (PDOException $e) {
-            $respuesta["error"] = "Imposible conectar:" . $e->getMessage();
-            return $respuesta;
-        }
-
-        try {
-            $consulta = "SELECT * FROM Usuarios WHERE id_usuario=?";
-            $sentencia = $conexion->prepare($consulta);
-            $sentencia->execute([$info->data]);
-        } catch (PDOException $e) {
-            $respuesta["error"] = "Imposible realizar la consulta:" . $e->getMessage();
-            $sentencia = null;
-            $conexion = null;
-            return $respuesta;
-        }
-
-        if ($sentencia->rowCount() > 0) {
-            $respuesta["usuario"] = $sentencia->fetch(PDO::FETCH_ASSOC);
-            $payload['exp'] = time() + 3600;
-            $payload['data'] = $respuesta["usuario"]["id_usuario"];
-            $jwt = JWT::encode($payload, PASSWORD_API, 'HS256');
-            $respuesta["token"] = $jwt;
-        } else {
-            $respuesta["mensaje_baneo"] = "El usuario no se encuentra registrado en la BD";
-        }
-
-        $sentencia = null;
-        $conexion = null;
-        return $respuesta;
-    }
-}
 
 function login($usuario, $contrasenya)
 {
@@ -82,9 +32,6 @@ function login($usuario, $contrasenya)
 
     if ($sentencia->rowCount() > 0) {
         $respuesta["usuario"] = $sentencia->fetch(PDO::FETCH_ASSOC);
-        //$payload = ['exp' => time() + 3600, 'data' => $respuesta["usuario"]["id_usuario"]];
-        //$jwt = JWT::encode($payload, PASSWORD_API, 'HS256');
-        //$respuesta["token"] = $jwt;
     } else {
         $respuesta["mensaje"] = "El usuario no se encuentra registrado en la BD";
     }
